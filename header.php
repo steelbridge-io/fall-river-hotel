5c19b51e91ac5b4235d327d1b4fac862af777caa
Chris Parsons <chris@parsonshosting.com>
2022-12-06 23:32:08 -0800

    added javascript for nav bg color change on scroll

--- header.php
+++ header.php
@@ -59,9 +59,9 @@
 			?>
 		</nav>--> <!-- #site-navigation -->
 
-        <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+        <nav id="frh-main-nav" class="navbar navbar-expand-md fixed-top navbar-dark bg-default">
+            <div class="container">
+                <a class="navbar-brand" href="#"><?php echo get_bloginfo('name'); ?></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -73,7 +73,7 @@
 						'container' => false,
 						'menu_class' => '',
 						'fallback_cb' => '__return_false',
-						'items_wrap' => '<ul id="%1$s" class="navbar-nav frh-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+						'items_wrap' => '<ul id="%1$s" class="navbar-nav frh-nav mb-2 mb-md-0 %2$s">%3$s</ul>',
 						'depth' => 2,
 						'walker' => new bootstrap_5_wp_nav_menu_walker()
 					));
--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,15 @@
 AOS.init();
+
+var $ = jQuery.noConflict(); // Add this to resolve $ undefined issue.
+
+$(document).ready(function() {
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 50) {
+            $('#frh-main-nav').addClass('bg-dark');
+            $('#frh-main-nav').removeClass('bg-trans');
+        } else if ($(this).scrollTop() < 50) {
+            $('#frh-main-nav').removeClass('bg-dark');
+            $('#frh-main-nav').addClass('bg-trans');
+        }
+    });
+});
