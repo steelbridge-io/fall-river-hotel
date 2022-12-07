a0812b71fcf372ee299ee151b0fc0f6fe891576e
Chris Parsons <chris@parsonshosting.com>
2022-12-06 22:25:47 -0800

    Added navwalker for bootstrap 5

--- functions.php
+++ functions.php
@@ -49,7 +49,8 @@ function fall_river_hotel_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'fall-river-hotel' ),
+			//'menu-1' => esc_html__( 'Primary', 'fall-river-hotel' ),
+			'main-menu' => esc_html__('Main menu', 'fall-river-hotel'),
 		)
 	);
 
@@ -99,8 +100,95 @@ function fall_river_hotel_setup() {
 			'flex-height' => true,
 		)
 	);
+
+	/**
+	 * Register Custom Navigation Walker
+	 */
+	if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
+		// File does not exist... return an error.
+		return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+	} else {
+		// File exists... require it.
+		return require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+	}
 }
 add_action( 'after_setup_theme', 'fall_river_hotel_setup' );
+
+/**
+ * bootstrap 5 wp_nav_menu walker
+ */
+class bootstrap_5_wp_nav_menu_walker extends Walker_Nav_menu {
+	private $current_item;
+	private $dropdown_menu_alignment_values = [
+		'dropdown-menu-start',
+		'dropdown-menu-end',
+		'dropdown-menu-sm-start',
+		'dropdown-menu-sm-end',
+		'dropdown-menu-md-start',
+		'dropdown-menu-md-end',
+		'dropdown-menu-lg-start',
+		'dropdown-menu-lg-end',
+		'dropdown-menu-xl-start',
+		'dropdown-menu-xl-end',
+		'dropdown-menu-xxl-start',
+		'dropdown-menu-xxl-end'
+	];
+
+	function start_lvl( &$output, $depth = 0, $args = null ) {
+		$dropdown_menu_class[] = '';
+		foreach ( $this->current_item->classes as $class ) {
+			if ( in_array( $class, $this->dropdown_menu_alignment_values ) ) {
+				$dropdown_menu_class[] = $class;
+			}
+		}
+		$indent  = str_repeat( "\t", $depth );
+		$submenu = ( $depth > 0 ) ? ' sub-menu' : '';
+		$output  .= "\n$indent<ul class=\"dropdown-menu$submenu " . esc_attr( implode( " ", $dropdown_menu_class ) ) . " depth_$depth\">\n";
+	}
+
+	function start_el( &$output, $item, $depth = 0, $args = null, $id = 0 ) {
+		$this->current_item = $item;
+
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		$li_attributes = '';
+		$class_names   = $value = '';
+
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+		$classes[] = ( $args->walker->has_children ) ? 'dropdown' : '';
+		$classes[] = 'nav-item';
+		$classes[] = 'nav-item-' . $item->ID;
+		if ( $depth && $args->walker->has_children ) {
+			$classes[] = 'dropdown-menu dropdown-menu-end';
+		}
+
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = ' class="' . esc_attr( $class_names ) . '"';
+
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args );
+		$id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$output .= $indent . '<li ' . $id . $value . $class_names . $li_attributes . '>';
+
+		$attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
+		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
+		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
+		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
+
+		$active_class   = ( $item->current || $item->current_item_ancestor || in_array( "current_page_parent", $item->classes, true ) || in_array( "current-post-ancestor", $item->classes, true ) ) ? 'active' : '';
+		$nav_link_class = ( $depth > 0 ) ? 'dropdown-item ' : 'nav-link ';
+		$attributes     .= ( $args->walker->has_children ) ? ' class="' . $nav_link_class . $active_class . ' dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="' . $nav_link_class . $active_class . '"';
+
+		$item_output = $args->before;
+		$item_output .= '<a' . $attributes . '>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+}
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
--- header.php
+++ header.php
@@ -26,6 +26,7 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'fall-river-hotel' ); ?></a>
 
 	<header id="masthead" class="site-header container-fluid">
+        <?php if(! is_front_page() || ! is_home()) : ?>
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
@@ -44,16 +45,44 @@
 				<p class="site-description"><?php echo $fall_river_hotel_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
+        <?php endif; ?>
 
-		<nav id="site-navigation" class="main-navigation">
+		<!--<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'fall-river-hotel' ); ?></button>
 			<?php
-			wp_nav_menu(
+			/*wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
 				)
-			);
+			);*/
 			?>
-		</nav><!-- #site-navigation -->
+		</nav>--> <!-- #site-navigation -->
+
+        <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="main-menu">
+					<?php
+					wp_nav_menu(array(
+						'theme_location' => 'main-menu',
+						'container' => false,
+						'menu_class' => '',
+						'fallback_cb' => '__return_false',
+						'items_wrap' => '<ul id="%1$s" class="navbar-nav frh-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+						'depth' => 2,
+						'walker' => new bootstrap_5_wp_nav_menu_walker()
+					));
+					?>
+                </div>
+            </div>
+        </nav>
+
+
+
+
 	</header><!-- #masthead -->
