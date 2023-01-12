68be57f88cdde0931eb15e5fd701d601a51833ac
steelbridge-io <chris@steelbridge.io>
2023-01-11 20:06:03 -0800

    Update to footer.php and associated CSS

--- footer.php
+++ footer.php
@@ -19,29 +19,18 @@
      <div class="col-lg-4 widget-cont">
       <?php dynamic_sidebar( 'footer_area_one' ); ?>
      </div>
-     <div class="col-lg-4 widget-cont">
+     <div class="col-lg-8 widget-cont">
       <?php dynamic_sidebar( 'footer_area_two' ); ?>
-     </div>
-     <div class="col-lg-4 widget-cont">
-      <?php dynamic_sidebar( 'footer_area_three' ); ?>
      </div>
     </div>
    </div>
   </div>
   
-		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'https://parsonshosting.com/store/wordpress-hosting', 'fall-river-hotel' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'fall-river-hotel' ), 'ParsonsHosting' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fall-river-hotel' ), 'fall-river-hotel', '<a href="https://parsonshosting.com">Dev Team at ParsonsHosting</a>' );
-				?>
-		</div><!-- .site-info -->
+  <div id="site-info-cont" class="container-fluid">
+   <div class="site-info container">
+    <span>Copyright&copy; <?php echo date("Y"); ?> Fall River Hotel</span>
+   </div><!-- .site-info -->
+  </div><!-- #site-info-cont -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
