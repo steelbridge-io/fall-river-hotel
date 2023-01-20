3ebf5abc40a8feb160a342923ef01c4a1c4726ef
steelbridge-io <chris@steelbridge.io>
2023-01-19 17:08:45 -0800

    Removed original nav code

--- header.php
+++ header.php
@@ -24,65 +24,26 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'fall-river-hotel' ); ?></a>
-
 	<header id="masthead" class="site-header container-fluid">
-        <?php if(! is_front_page() || ! is_home()) : ?>
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$fall_river_hotel_description = get_bloginfo( 'description', 'display' );
-			if ( $fall_river_hotel_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $fall_river_hotel_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-        <?php endif; ?>
-
-		<!--<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'fall-river-hotel' ); ?></button>
-			<?php
-			/*wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);*/
-			?>
-		</nav>--> <!-- #site-navigation -->
-
-        <nav id="frh-main-nav" class="navbar navbar-expand-md fixed-top navbar-dark bg-default">
-            <div class="container">
-                <a class="navbar-brand" href="#"><?php echo get_bloginfo('name'); ?></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="main-menu">
-					<?php
-					wp_nav_menu(array(
-						'theme_location' => 'main-menu',
-						'container' => false,
-						'menu_class' => '',
-						'fallback_cb' => '__return_false',
-						'items_wrap' => '<ul id="%1$s" class="navbar-nav frh-nav mb-2 mb-md-0 %2$s">%3$s</ul>',
-						'depth' => 2,
-						'walker' => new bootstrap_5_wp_nav_menu_walker()
-					));
-					?>
-                </div>
-            </div>
-        </nav>
-
-
-
-
+  <nav id="frh-main-nav" class="navbar navbar-expand-md fixed-top navbar-dark bg-default">
+      <div class="container">
+          <a class="navbar-brand" href="#"><?php echo get_bloginfo('name'); ?></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="main-menu">
+           <?php
+           wp_nav_menu(array(
+           'theme_location' => 'main-menu',
+           'container' => false,
+           'menu_class' => '',
+           'fallback_cb' => '__return_false',
+           'items_wrap' => '<ul id="%1$s" class="navbar-nav frh-nav mb-2 mb-md-0 %2$s">%3$s</ul>',
+           'depth' => 2,
+           'walker' => new bootstrap_5_wp_nav_menu_walker()
+           ));
+           ?>
+          </div>
+      </div>
+  </nav>
 	</header><!-- #masthead -->
