716c917a0fc63ef8e0d4fc61df11858376a1498d
Chris Parsons <chris@parsonshosting.com>
2023-01-24 22:03:56 -0800

    New template

---
+++ templates/new-default.php
@@ -0,0 +1,46 @@
+<?php
+	/**
+	 * Template Name: New Default
+	 *
+	 * This is the template that displays all pages by default.
+	 * Please note that this is the WordPress construct of pages
+	 * and that other 'pages' on your WordPress site may use a
+	 * different template.
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+	 *
+	 * @package Fall_River_Hotel
+	 */
+
+	get_header();
+?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<main id="primary" class="site-main">
+
+					<?php
+						while ( have_posts() ) :
+							the_post();
+
+							get_template_part( 'template-parts/content', 'newDefault' );
+
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+
+						endwhile; // End of the loop.
+					?>
+
+				</main><!-- #main -->
+			</div>
+
+			<div class="col-md-4">
+				<?php
+					get_sidebar(); ?>
+			</div>
+		</div>
+	</div>
+<?php
+	get_footer();
