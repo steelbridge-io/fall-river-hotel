c10b7d336aefaf7280b35ce8034f115c5db2e6a5
Chris Parsons <chris@parsonshosting.com>
2023-01-24 21:02:30 -0800

    Added recreation-template.php

--- templates/recreation-template.php
+++ templates/recreation-template.php
@@ -20,19 +20,19 @@
 	 */
 
 ?>
-
-  <div id="wide-temp-cont" class="p-5 mb-4 bg-light rec-temp-bg-img">
-    <div class="container py-5 rec-temp-img">
-    <?php the_title( '<h1>', '</h1>' ); ?>
+<div class="banner">
+  <div id="wide-temp-cont" class="p-5 mb-4 bg-light rec-temp-bg-img"></div>
+  <div class="container py-5 rec-temp-img">
+		<?php the_title( '<h1>', '</h1>' ); ?>
     <p class="col-md-8 fs-4"><?php echo $hero_cta_content; ?></p>
     <div class="hero-read-more-icon">
       <div id="section1" class="scrollto animated animatedFadeInUp fadeInUp" onclick="scrolldiv()">
-      <a href="#section2"><i class="lni lni-chevron-down-circle"></i>&nbsp;<span
-      class="cta-click-prompt"><?php echo $scroll_to_cta; ?></span></a>
+        <a href="#section2"><i class="lni lni-chevron-down-circle"></i>&nbsp;<span
+            class="cta-click-prompt"><?php echo $scroll_to_cta; ?></span></a>
       </div>
     </div>
-    </div>
   </div>
+</div>
 
   <div id="section2" class="main second"></div>
 
